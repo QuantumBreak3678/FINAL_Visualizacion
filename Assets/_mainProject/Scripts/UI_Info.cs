fc379f6cf3e8867772f6794b71f589511b409548
juandagongoradiaz <68030722+QuantumBreak3678@users.noreply.github.com>
2023-11-02 22:29:24 -0500

    a

--- Assets/_mainProject/Scripts/UI_Info.cs
+++ Assets/_mainProject/Scripts/UI_Info.cs
@@ -6,26 +6,30 @@ using DG.Tweening;
 public class UI_Info : MonoBehaviour
 {
     public GameObject UI_Infographic;
-    public Vector3 vector; 
- 
+
+    public InterfaceAnimManager InterfaceAnimManager; 
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
            
+            InterfaceAnimManager.startAppear();
             UI_Infographic.SetActive(true);
-            UI_Infographic.transform.DOScale(vector, 0.5f); 
 
         }
     }
-    
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            UI_Infographic.transform.DOScale(new Vector3(0,0,0), 0.5f); 
-            UI_Infographic.SetActive(false);
+         
+           
+            InterfaceAnimManager.startDisappear();
+            
           
         }
     }
+   
+    
 }
