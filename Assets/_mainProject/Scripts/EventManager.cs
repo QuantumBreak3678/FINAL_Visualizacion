1e31414d1706e19963cc6cede5781c5229eda287
juandagongoradiaz <68030722+QuantumBreak3678@users.noreply.github.com>
2023-11-14 00:47:08 -0500

    dfdf

--- Assets/_mainProject/Scripts/EventManager.cs
+++ Assets/_mainProject/Scripts/EventManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using DG.Tweening; 
 using UnityEngine.Playables;
@@ -7,15 +8,28 @@ public class EventManager : MonoBehaviour
 {
     public PlayableDirector MainStart; 
     public PlayableDirector MachineCinematic; 
+    public PlayableDirector EndCinematic; 
 
     public void StartGame()
     {
         MainStart.Play();
     }
-    
+
     public void StartCinematic()
     {
         MachineCinematic.Play();
     }
-    
+
+    public void EndCinematicFUCTION()
+    {
+        EndCinematic.Play();
+        StartCoroutine(END()); 
+    }
+
+    IEnumerator END()
+    {
+        yield return new WaitForSeconds(2f);
+
+    }
+
 }
